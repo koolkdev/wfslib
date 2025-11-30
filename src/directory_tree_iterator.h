c778b310f74e0ac86f08d0b95bb42c8c269fe164
koolkdev <koolkdev@gmail.com>
2025-11-30 12:04:52 +0200

    DirectoryTreeIterator: avoid ranges to pipe syntax

--- src/directory_tree_iterator.h
+++ src/directory_tree_iterator.h
@@ -158,11 +158,13 @@ class DirectoryTreeIterator {
 
  private:
   std::string key() const {
-    return (parents_ | std::views::transform([](const auto& parent) {
-              return std::ranges::to<std::string>(parent.node.prefix()) + (*parent.iterator).key();
-            }) |
-            std::views::join | std::ranges::to<std::string>()) +
-           std::ranges::to<std::string>(leaf_->get_node().prefix());
+    auto parents_key = std::ranges::to<std::string>(
+        parents_ | std::views::transform([](const auto& parent) {
+          return std::ranges::to<std::string>(parent.node.prefix()) + (*parent.iterator).key();
+        }) |
+        std::views::join);
+
+    return parents_key + std::ranges::to<std::string>(leaf_->get_node().prefix());
   };
 
   Block* block_;
