dbcdef1427d847e7e21e3e772d0a404cc1c65468
koolkdev <koolkdev@gmail.com>
2025-11-30 11:50:02 +0200

    DirectoryTree: replace ranges to pipes for clang 21

--- src/directory_tree_iterator.h
+++ src/directory_tree_iterator.h
@@ -159,10 +159,10 @@ class DirectoryTreeIterator {
  private:
   std::string key() const {
     return (parents_ | std::views::transform([](const auto& parent) {
-              return (parent.node.prefix() | std::ranges::to<std::string>()) + (*parent.iterator).key();
+              return std::ranges::to<std::string>(parent.node.prefix()) + (*parent.iterator).key();
             }) |
             std::views::join | std::ranges::to<std::string>()) +
-           (leaf_->get_node().prefix() | std::ranges::to<std::string>());
+           std::ranges::to<std::string>(leaf_->get_node().prefix());
   };
 
   Block* block_;
